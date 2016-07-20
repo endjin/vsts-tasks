7e72281f7559a7051edc3e5805c00547f8f4dbfb
Jason Sholl <jasholl@microsoft.com>
2016-07-20 14:16:22 -0400

    JenkinsQueueJob: split task into several modules

---
+++ Tasks/JenkinsQueueJob/util.ts
@@ -0,0 +1,37 @@
+/// <reference path="../../definitions/node.d.ts"/>
+import tl = require('vsts-task-lib/task');
+
+export function failReturnCode(httpResponse, message: string): void {
+    var fullMessage = message +
+        '\nHttpResponse.statusCode=' + httpResponse.statusCode +
+        '\nHttpResponse.statusMessage=' + httpResponse.statusMessage +
+        '\nHttpResponse=\n' + JSON.stringify(httpResponse);
+    fail(fullMessage);
+}
+
+export function handleConnectionResetError(err): void {
+    if (err.code == 'ECONNRESET') {
+        tl.debug(err);
+    } else {
+        fail(err);
+    }
+}
+
+export function fail(message: string): void {
+    throw new FailTaskError(message);
+}
+
+export class FailTaskError extends Error {
+}
+
+export function addUrlSegment(baseUrl: string, segment: string): string {
+    var resultUrl = null;
+    if (baseUrl.endsWith('/') && segment.startsWith('/')) {
+        resultUrl = baseUrl + segment.slice(1);
+    } else if (baseUrl.endsWith('/') || segment.startsWith('/')) {
+        resultUrl = baseUrl + segment;
+    } else {
+        resultUrl = baseUrl + '/' + segment;
+    }
+    return resultUrl;
+}
